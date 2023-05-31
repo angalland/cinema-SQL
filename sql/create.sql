c94f4cbabc7bd8141c39fc1c5f69f62ef7a6f15f
angalland <angalland@laposte.net>
2023-05-31 09:55:43 +0200

    modification create_table

--- sql/create.sql
+++ sql/create.sql
@@ -1,33 +1,46 @@
-CREATE TABLE IF NOT EXISTS Realisateur(
-   id_realisateur INT AUTO_INCREMENT,
-   nom_realisateur VARCHAR(25) NOT NULL,
-   prenom VARCHAR(25) NOT NULL,
-   date_naissance_realisateur DATE NOT NULL,
-   sexe_realisateur VARCHAR(1) NOT NULL,
-   PRIMARY KEY(id_realisateur)
-);
-
-CREATE TABLE IF NOT EXISTS Genre(
-   id_genre INT AUTO_INCREMENT,
+CREATE TABLE Genre(
+   id_genre INT,
    nom_genre VARCHAR(25) NOT NULL,
    PRIMARY KEY(id_genre)
 );
 
-CREATE TABLE IF NOT EXISTS Acteur(
-   id_acteur INT AUTO_INCREMENT,
-   nom_acteur VARCHAR(25) NOT NULL,
-   prenom_acteur VARCHAR(25) NOT NULL,
-   sexe_acteur VARCHAR(1) NOT NULL,
-   date_naissance_acteur DATE NOT NULL,
-   PRIMARY KEY(id_acteur)
+CREATE TABLE personne(
+   id_personne INT,
+   nom VARCHAR(25) NOT NULL,
+   prenom VARCHAR(25) NOT NULL,
+   date_naissance DATE NOT NULL,
+   sexe VARCHAR(1) NOT NULL,
+   PRIMARY KEY(id_personne)
 );
 
-CREATE TABLE IF NOT EXISTS Film(
-   id_film INT AUTO_INCREMENT,
+CREATE TABLE role(
+   id_role INT,
+   nom_role VARCHAR(25) NOT NULL,
+   PRIMARY KEY(id_role)
+);
+
+CREATE TABLE Realisateur(
+   id_realisateur INT,
+   id_personne INT NOT NULL,
+   PRIMARY KEY(id_realisateur),
+   UNIQUE(id_personne),
+   FOREIGN KEY(id_personne) REFERENCES personne(id_personne)
+);
+
+CREATE TABLE Acteur(
+   id_acteur INT,
+   id_personne INT NOT NULL,
+   PRIMARY KEY(id_acteur),
+   UNIQUE(id_personne),
+   FOREIGN KEY(id_personne) REFERENCES personne(id_personne)
+);
+
+CREATE TABLE Film(
+   id_film INT,
+   duree_film TIME NOT NULL,
    titre VARCHAR(25) NOT NULL,
    date_sortie DATE NOT NULL,
-   duree_film TIME NOT NULL,
-   synopsis TEXT(500),
+   synopsis TEXT COLLATE 500,
    note INT,
    affiche VARCHAR(50),
    id_realisateur INT NOT NULL,
@@ -35,7 +48,7 @@ CREATE TABLE IF NOT EXISTS Film(
    FOREIGN KEY(id_realisateur) REFERENCES Realisateur(id_realisateur)
 );
 
-CREATE TABLE IF NOT EXISTS posseder_genre(
+CREATE TABLE posseder_genre(
    id_film INT,
    id_genre INT,
    PRIMARY KEY(id_film, id_genre),
@@ -43,11 +56,13 @@ CREATE TABLE IF NOT EXISTS posseder_genre(
    FOREIGN KEY(id_genre) REFERENCES Genre(id_genre)
 );
 
-CREATE TABLE IF NOT EXISTS jouer(
+CREATE TABLE jouer(
    id_film INT,
    id_acteur INT,
-   role VARCHAR(25),
-   PRIMARY KEY(id_film, id_acteur),
+   id_role INT,
+   PRIMARY KEY(id_film, id_acteur, id_role),
    FOREIGN KEY(id_film) REFERENCES Film(id_film),
-   FOREIGN KEY(id_acteur) REFERENCES Acteur(id_acteur)
+   FOREIGN KEY(id_acteur) REFERENCES Acteur(id_acteur),
+   FOREIGN KEY(id_role) REFERENCES role(id_role)
 );
+
