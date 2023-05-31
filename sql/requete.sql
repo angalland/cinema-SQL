db8eddc35213603736a568c1cd20e0d187544280
angalland <angalland@laposte.net>
2023-05-31 11:22:16 +0200

    ajout insert personne

--- sql/requete.sql
+++ sql/requete.sql
@@ -10,5 +10,25 @@ VAlUES  ('science fiction'),
         ('fantastique'),
         ('horreur');
 
-INSERT INTO acteur (nom_acteur, prenom_acteur, sexe_acteur, date_naissance_acteur)
-VAlUES  ()   
+INSERT INTO personne (nom, prenom, date_naissance, sexe)
+VAlUES  ('Ford', 'Harrison', '1942-07-13', 'M' ),
+        ('Allen', 'Karen', '1951-10-05', 'F'),
+        ('Hamil', 'Mark', '1951-09-25', 'M'),
+        ('Fisher', 'Carrie', '1956-10-21', 'F'),
+        ('Le Besco', 'Maïwenn', '1976-04-17', 'F'),
+        ('Viard', 'Karin', '1966-01-24', 'F'),
+        ('Starr', 'Joey', '1967-10-27', 'M'),
+        ('Foïs', 'Marina', '1970-01-21', 'F'),
+        ('Dubosc', 'Franck', '1963-11-07', 'M'),
+        ('Lanvain', 'Gérard', '1950-06-21', 'M'),
+        ('Seigner', 'Mathilde', '1968-01-17', 'F'),
+        ('Sullivan', 'Lily', '1994-09-08', 'F'),
+        ('Sutherland', 'Alyssa', '1982-06-23', 'F'),
+        ('Pratt', 'Cristopher', '1979-06-21', 'M'),
+        ('Day', 'Charlie', '1976-02-09', 'M'),
+        ('Taylor-Joy', 'Anya', '1996-04-16', 'F')
+        ('Black', 'Jack', '1969-08-28', 'M');
+
+
+
+
